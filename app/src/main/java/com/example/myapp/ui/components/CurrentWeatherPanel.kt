1173b2466829d4a56c9af58b32cd85be9544a40a
Per-Henrik <perhenrik.kvalnes@gmail.com>
2026-04-20 18:16:46 +0200

    add tempratrue, description and wind to current weather panel

--- app/src/main/java/com/example/myapp/ui/components/CurrentWeatherPanel.kt
+++ app/src/main/java/com/example/myapp/ui/components/CurrentWeatherPanel.kt
@@ -1,16 +1,25 @@
 package com.example.myapp.ui.components
 
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.material3.Text
+
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import com.example.myapp.data.models.CurrentWeather
+import com.example.myapp.utils.weatherDescription
 
 @Composable
 fun CurrentWeatherPanel (currentWeather: CurrentWeather){
     Box(
         contentAlignment = Alignment.Center,
     ) {
-        WeatherIcon(currentWeather.weathercode)
+        Column() {
+            WeatherIcon(currentWeather.weathercode)
+            Text("${currentWeather.temperature} °")
+            Text(weatherDescription(currentWeather.weathercode))
+            Text("Vind ${currentWeather.windspeed} km/t")
+        }
+
     }
 }
