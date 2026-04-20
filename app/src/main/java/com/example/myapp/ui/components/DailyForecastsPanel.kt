95837ce8d09ef8a4d3b22d977946e4882b6abe31
Per-Henrik <perhenrik.kvalnes@gmail.com>
2026-04-20 20:48:51 +0200

    add day forecast row

--- app/src/main/java/com/example/myapp/ui/components/DailyForecastsPanel.kt
+++ app/src/main/java/com/example/myapp/ui/components/DailyForecastsPanel.kt
@@ -1,15 +1,26 @@
 package com.example.myapp.ui.components
 
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import com.example.myapp.data.models.DayForecast
+import com.example.myapp.utils.formatDate
 
 @Composable
 fun DailyForecastsPanel(dailyForcasts: List<DayForecast>) {
     Column() {
         dailyForcasts.map {
-            Text(it.time)
+            DayForecastRow(it)
         }
+    }
+}
+
+@Composable
+fun DayForecastRow(dayForecast: DayForecast){
+    Row() {
+        Text(formatDate(dayForecast.time))
+        WeatherIcon(dayForecast.weatherCode)
+        Text("${dayForecast.minTemperature}° - ${dayForecast.maxTemperature}°")
     }
 }
