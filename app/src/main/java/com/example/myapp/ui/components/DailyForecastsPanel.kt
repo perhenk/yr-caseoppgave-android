8b3c5cfa52a27481badd31ca599da321f98aee1c
Per-Henrik <perhenrik.kvalnes@gmail.com>
2026-04-20 21:02:14 +0200

    add scrollable lazy column and spacer

--- app/src/main/java/com/example/myapp/ui/components/DailyForecastsPanel.kt
+++ app/src/main/java/com/example/myapp/ui/components/DailyForecastsPanel.kt
@@ -2,25 +2,33 @@ package com.example.myapp.ui.components
 
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import com.example.myapp.data.models.DayForecast
 import com.example.myapp.utils.formatDate
 
 @Composable
 fun DailyForecastsPanel(dailyForcasts: List<DayForecast>) {
-    Column() {
-        dailyForcasts.map {
-            DayForecastRow(it)
+    LazyColumn(modifier= Modifier.fillMaxSize()) {
+        items(dailyForcasts){data -> DayForecastRow(data)}
         }
-    }
 }
+
 
 @Composable
 fun DayForecastRow(dayForecast: DayForecast){
-    Row() {
-        Text(formatDate(dayForecast.time))
+    Row {
+        Text(formatDate(dayForecast.time), modifier = Modifier.padding(16.dp))
         WeatherIcon(dayForecast.weatherCode)
-        Text("${dayForecast.minTemperature}° - ${dayForecast.maxTemperature}°")
+        Spacer(modifier = Modifier.weight(1f))
+        Text("${dayForecast.minTemperature}° - ${dayForecast.maxTemperature}°",  modifier = Modifier.padding(16.dp))
     }
 }
