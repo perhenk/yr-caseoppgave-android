783e1793d7153ce49c93ad241d68cbda1d1d54f8
Per-Henrik <perhenrik.kvalnes@gmail.com>
2026-04-21 15:03:55 +0200

    add serialized name

--- app/src/main/java/com/example/myapp/data/models/CurrentWeather.kt
+++ app/src/main/java/com/example/myapp/data/models/CurrentWeather.kt
@@ -1,11 +1,16 @@
 package com.example.myapp.data.models
 
+import com.google.gson.annotations.SerializedName
+
 data class CurrentWeather(
     val time: String,
     val interval: Int,
     val temperature: Double,
     val windspeed: Double,
-    val is_day: Int,
-    val weathercode: Int,
+
+    @SerializedName("is_day")
+    val isDay: Int,
+    @SerializedName("weatherCode")
+    val weatherCode: Int,
     val winddirection: Int
 )
--- app/src/main/java/com/example/myapp/data/models/DayForecast.kt
+++ app/src/main/java/com/example/myapp/data/models/DayForecast.kt
@@ -1,9 +1,15 @@
 package com.example.myapp.data.models
 
+import com.google.gson.annotations.SerializedName
+
 data class DayForecast(
     val time: String,
+    @SerializedName("temperature_2m_max")
     val maxTemperature: Float,
+    @SerializedName("temperature_2m_min")
     val minTemperature: Float,
+    @SerializedName("weathercode")
     val weatherCode: Int,
+    @SerializedName("precipitation_sum")
     val precipitation: Double
 )
