a904a27dceedd77b2ad6b9d44073d7388579c96f
Per-Henrik <perhenrik.kvalnes@gmail.com>
2026-04-21 19:06:38 +0200

    larger weather icon on detail site

--- app/src/main/java/com/example/myapp/ui/components/DayForecastDetails.kt
+++ app/src/main/java/com/example/myapp/ui/components/DayForecastDetails.kt
@@ -9,6 +9,7 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 
 import androidx.compose.material3.Card
 
@@ -39,8 +40,8 @@ fun DayForecastDetails(navController: NavController, dayForecast: DayForecast) {
         }
 
         Column(horizontalAlignment = Alignment.CenterHorizontally, modifier = Modifier.fillMaxWidth()) {
-            WeatherIcon(dayForecast.weatherCode)
-            Text(weatherDescription(dayForecast.weatherCode), fontSize = 30.sp)
+            WeatherIcon(dayForecast.weatherCode, modifier = Modifier.size(100.dp))
+            Text(weatherDescription(dayForecast.weatherCode), fontSize = 30.sp, modifier = Modifier.padding(20.dp))
         }
 
         Card(modifier = Modifier.fillMaxWidth().padding(10.dp)) {
