cfdabaa83762dc0110780438996d72f125ced5c0
Per-Henrik <perhenrik.kvalnes@gmail.com>
2026-04-24 12:03:53 +0200

    query parameters on repo

--- app/src/main/java/com/example/myapp/data/service/WeatherService.kt
+++ app/src/main/java/com/example/myapp/data/service/WeatherService.kt
@@ -2,8 +2,17 @@ package com.example.myapp.data.service
 
 import com.example.myapp.data.models.WeatherForecast
 import retrofit2.http.GET
+import retrofit2.http.Query
+
 
 interface WeatherService {
-    @GET("forecast?latitude=67.28&longitude=14.38&daily=temperature_2m_max,temperature_2m_min,weathercode,precipitation_sum&current_weather=true&timezone=Europe%2FOslo&models=metno_seamless")
-    suspend fun getForecast(): WeatherForecast
+    @GET("forecast")
+    suspend fun getForecast(
+        @Query("latitude") latitude: Double = 59.91,
+        @Query("longitude") longitude: Double = 10.75,
+        @Query("daily") daily: String = "temperature_2m_max,temperature_2m_min,weathercode,precipitation_sum",
+        @Query("current_weather") currentWeather: Boolean = true,
+        @Query("timezone") timezone: String = "Europe/Oslo",
+        @Query("models") models: String = "metno_seamless"
+    ): WeatherForecast
 }
